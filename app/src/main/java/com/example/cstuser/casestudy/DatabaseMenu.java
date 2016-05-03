8e0dc31466b8b7eaa7566e48e406b11ff694cd6c
sherif ghaffar <sherif-99@hotmail.com>
2016-05-02 20:35:04 -0400

    eyo
    Signed-off-by: sherif ghaffar <sherif-99@hotmail.com>

---
+++ app/src/main/java/com/example/cstuser/casestudy/DatabaseMenu.java
@@ -0,0 +1,17 @@
+package com.example.cstuser.casestudy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sherif on 2016-05-01.
+ */
+public class DatabaseMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.database_menu_ui);
+
+
+    }
+}
